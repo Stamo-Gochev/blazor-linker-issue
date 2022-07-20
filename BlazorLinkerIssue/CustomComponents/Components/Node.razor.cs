edac6449ef78c4a26758d65f982dbd702a0ed603
Stamo-Gochev <stamo_gochev@yahoo.com>
2022-07-20 08:24:04 +0300

    feat: add component library

---
+++ BlazorLinkerIssue/CustomComponents/Components/Node.razor.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomComponents.Components
+{
+    public partial class Node : ComponentBase
+    {
+        [Parameter]
+        public bool RenderNext { get; set; } = true;
+    }
+}
